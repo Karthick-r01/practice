751688b4a1d7daed3d10283efae2aa376c7580ce
Karthick-r01 <140738567+Karthick-r01@users.noreply.github.com>
2023-10-28 09:43:59 +0530

    Add files via upload

---
+++ struct.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+struct student {
+    int Id;
+    char* name;
+    char* dep;
+};
+
+void main() {
+    struct student s1;
+    s1.Id=15;
+    s1.name="student15";
+    s1.dep="computer science";
+
+    printf("%d\n", s1.Id);
+    printf("%s\n", s1.name);
+    printf("%s\n", s1.dep);
+}
